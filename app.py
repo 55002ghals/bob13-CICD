ef7af1f0c660161eda85adc9c0106ae9e01fcff6
55002ghals <ab5502ghals@naver.com>
2025-02-15 21:21:51 +0900

    testforCICD

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "BoB13. jmh, swb22222222"
+    return "BoB13. jmh, swb3333333"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
